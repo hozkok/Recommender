b6d37de5b99dbd0b7465322626eb9af0ceecbefa
zeawee <zeawee@users.noreply.github.com>
2015-07-29 18:24:07 +0100

    bug fix

---
+++ client/platforms/android/build/intermediates/assets/debug/www/js/db-config.js
@@ -0,0 +1,40 @@
+recommender.constant('DB_CONF', {
+    name: 'recommend.db',
+    tables: [
+        {
+            name: 'user',
+            attrs: [
+                {name: 'id', type: 'text unique'},
+                {name: 'name', type: 'text'},
+                {name: 'phone', type: 'text unique'},
+                {name: 'push_token', type: 'text unique'}
+            ]
+        },
+        {
+            name: 'contacts',
+            attrs: [
+                {name: 'name', type: 'text'},
+                {name: 'phone', type: 'text unique'}
+            ]
+        },
+        {
+            name: 'topics',
+            attrs: [
+                {name: 'id', type: 'text unique'},
+                {name: 'owner_name', type: 'text'},
+                {name: '`where`', type: 'text'},
+                {name: '`when`', type: 'text'},
+                {name: 'description', type: 'text'}
+            ]
+        },
+        {
+            name: 'messages',
+            attrs: [
+                {name: 'topic_id', type: 'text'},
+                {name: 'text', type: 'text'},
+                {name: 'sender', type: 'text'},
+                {name: 'date', type: 'datetime'}
+            ]
+        }
+    ]
+});
